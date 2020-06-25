1b529a132d26eb4106827933c38a60d95e6278fa
butterfly1of4 <jhouck29@gmail.com>
2020-06-25 16:16:42 -0400

    hands dealt

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Game-of-War
 
-A 2 player card game using all 52 cards. Each player gets half the deck. The players flip cards simulatneously and compare numbers. Highest number wins. If Player 1 & Player 2's numbers match, War is declared. For war, each player deals three cards down and one card up. The higher number of the overtuned cards wins. In the event of a tie, the next card is flipped, and so on until there is a winner. Winner takes all the cards and adds them to the bottom of their deck. 
+A 2 player card game using all 52 cards. Each player gets half the deck. The players flip cards simulatneously and compare numbers. Highest number wins. If Player 1 & Player 2's numbers match, War is declared. For war, each player deals three cards down and one card up. The higher number of the overtuned cards wins. In the event of a tie, the next card is flipped, and so on until there is a winner. Winner takes all the cards and adds them to the bottom of their deck.
 
 ##Start game
 -open [https://github.com/butterfly1of4/Game-of-War/blob/master/index.html]: https://github.com/butterfly1of4/Game-of-War/blob/master/index.html
 
-``` 
-startGame() return Boolean 
+```
+startGame() return Boolean
 
 ```
 
 ##Create Deck:
 
-```const Deck = []```, 
+`const Deck = []`,
 
 ##Declare players:
+
 - create 2 players
-```
+
+````
 const player = {
     hand= []
 }```
@@ -43,17 +45,17 @@ For:
     winner takes cards
     ``` addCardsToHand()```
 
-    if the cards match: 
+    if the cards match:
     ```declareWar()```
 
-    if a player is out of cards 
+    if a player is out of cards
     ```gameEnds()```
 
     and the winner is declared,
     and another game may be initiated
     ```playAgain()```
         resume play
-*same 
+*same
     War is declared
 
 
@@ -65,9 +67,9 @@ deal 3 cards face down
 ```dealFaceDown()```
 and one face up
 ```flipCard()```
-cards are compared 
+cards are compared
 ```evaluateCards()```
-if there's a winner, cards are added to their deck 
+if there's a winner, cards are added to their deck
 ```addCardsToDeck()```
 game continues as usual
 
@@ -77,3 +79,4 @@ if there is a tie, the first face down card is flipped
 ```flipFaceDown()```
 ```evaluateCards()```
 if a tie remains, same 2 functions.
+````
--- wargame.js
+++ wargame.js
@@ -1,60 +1,75 @@
-console.log("i'm connective")
+console.log("i'm connective");
 //Players
-const player1 = {hand:[]};
-const player2 = {hand:[]}
-let deck = [{one: 'one'}, {two: 'two'}, {potato: 'potato'}, {sunday: 'Sunday'}]
+const player1 = { hand: [] };
+const player2 = { hand: [] };
+let testArr = [
+  { one: "one" },
+  { two: "two" },
+  { potato: "potato" },
+  { sunday: "Sunday" },
+];
 //Cards/deck
-const suits= ["Hearts", "Clubs", "Spades","Diamonds"];
-const cards= ["two","three","four","five","six","seven","eight","nine","ten","Jack","Queen","King","Ace"];
-const value= [2,3,4,5,6,7,8,9,10,11,12,13,14];
-const deck = []
-let card = {}
-let shuffledDDeck = []
+const suits = ["Hearts", "Clubs", "Spades", "Diamonds"];
+const cards = [
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "Jack",
+  "Queen",
+  "King",
+  "Ace",
+];
+const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const deck = [];
+let card = {};
+let shuffledDDeck = [];
 
 //test variables
 
-
 //Create deck
-function createDeck(){
-    for (let i = 0; i < suits.length; i++){
-        for(let j = 0; j< cards.length; j++){
-       card ={
-            suits: suits[i],
-            cards: cards[j],
-            value: value[j]
-        }
-       deck.push(card)
+function createDeck() {
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < cards.length; j++) {
+      card = {
+        suits: suits[i],
+        cards: cards[j],
+        value: value[j],
+      };
+      deck.push(card);
     }
-        	
-        
-		 		console.log(deck)
-    } return deck
-} 
-console.log(deck);
-createDeck()
+  }
+  console.log(deck);
+  return deck;
+}
+
+createDeck();
+
 //Shuffle
 
-
-
-function shuffle(deck){
-    for (let i =deck.length - 1; i >0;i--){
-        let j = Math.floor(Math.random()* (i+1));
-        let temp = deck[i]
-        deck[i] = deck[j];
-        deck[j]= temp;
-    }
-    return deck
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+  return deck;
 }
-shuffle(deck)
-
-
+shuffle(deck);
 
 //Deal
-for (let i = 0; i < 26; i++){
-    player1.hand.push(deck[i])
-}
-console.log(player1.hand)
+for (let i = 0; i < 26; i++) {
+  player1.hand.push(deck[i]);
+} 
+console.log(player1.hand);
 
-
-
-
+for (let i = 26; i < 52; i++) {
+  player2.hand.push(deck[i]);
+} 
+console.log(player2.hand);
