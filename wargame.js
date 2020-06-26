8cdb2a563a9f7834fb8f95e4216bac5fdaaf18b1
butterfly1of4 <jhouck29@gmail.com>
2020-06-26 09:31:26 -0400

    war setup

--- wargame.js
+++ wargame.js
@@ -27,6 +27,7 @@ const deck = [];
 let card = {};
 let shuffledDDeck = [];
 let arena = []
+let battlefield = [[],[]]
 
 //Create deck
 function createDeck() {
@@ -63,43 +64,71 @@ shuffle(deck);
 for (let i = 0; i < 26; i++) {
   player1.hand.push(deck[i]);
 } 
-console.log(player1.hand);
 
 for (let i = 26; i < 52; i++) {
   player2.hand.push(deck[i]);
 } 
-console.log(player2.hand);
-console.log(player2.hand)
+//console.log(player2.hand);
+//console.log(player2.hand)
 
 
 //Play a hand
 function playCard(card1){
-  let x = card1.shift(0)
+  let x = card1.shift()
   arena.push(x)
   return arena
  }
 playCard(player1.hand)
 playCard(player2.hand)
-console.log(arena)
+//console.log(arena)
 
-
+/*
 //Compare cards
 function compareCards(){
   if (arena[0].value > arena[1].value) {
-    player1.collectCards()
+    player1.hand.push(...arena)
     console.log("Player 1 wins")
   } else if(arena[0].value < arena[1].value){
-    player2.collectCards()
+    player2.hand.push(...arena)
     console.log("Player 2 wins")
   } else if(arena[0].value == arena[1].value){
-    //console.log("We have a war")
-    return "To the arena"
+    console.log("We have a war")
+
   }
   console.log(player2.hand, player1.hand)
 }
 compareCards()
 
-console.log(arena, player2, player1)
-function pickUp(params) {
-  
-}
+class War {
+  constructor(field) {
+    this.battlefield = battlefield
+  }
+  dealWar() {
+  }}*/
+arena[1]== arena[0]
+//WAR
+function cardsDownWar(card2){
+  let y = card2.shift()
+  battlefield[1].push(y)
+  }
+
+  function cardsUpWar(card3) {
+    let y = card3.shift()
+    battlefield[0].push(y);
+  }
+
+  cardsDownWar(player2.hand)
+  cardsDownWar(player2.hand)
+  cardsUpWar(player1.hand)
+  cardsUpWar(player2.hand)
+  console.log(battlefield, player1.hand, player2.hand)
+
+  function gameOver(hand1,hand2) {
+    if (player1.hand == 0) {
+      console.log("Game over. Player 2 wins")
+    } else if (player2.hand == 0){
+      console.log("Game over. Player 1 wins")
+    } else {
+      
+    }
+  }
